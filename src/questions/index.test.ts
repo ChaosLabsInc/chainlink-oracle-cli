0ac933c562a8f585698c5c912952159b7b968974
Omer <arieg419@gmail.com>
2021-11-02 12:40:19 -0600

    feat: added first q tests

--- src/questions/index.test.ts
+++ src/questions/index.test.ts
@@ -1,11 +1,27 @@
+import Questions from "./index";
 const QUESTION_TEST_SUITE = "QUESTION_TEST_SUITE ";
 
 test(`${QUESTION_TEST_SUITE} - getConfigurablePriceFeedsQuestion`, () => {
-  expect("1").toBe("1");
+  const configPFQuestions = Questions.getConfigurablePriceFeedsQuestion([]);
+  expect(configPFQuestions.length).toBeGreaterThan(0);
+  expect(configPFQuestions[0]).toMatchObject({
+    type: "rawlist",
+    name: Questions.QUESTION_NAMES.CONFIGURABLE_FEEDS,
+    message: "Select price feed:",
+    choices: [],
+    default: [],
+  });
 });
 
 test(`${QUESTION_TEST_SUITE} - getAllPriceFeedsQuestion`, () => {
-  expect("1").toBe("1");
+  const PFQuestions = Questions.getAllPriceFeedsQuestion(["abc"]);
+  expect(PFQuestions.length).toBeGreaterThan(0);
+  expect(PFQuestions[0]).toMatchObject({
+    type: "rawlist",
+    name: Questions.QUESTION_NAMES.CONFIGURABLE_FEEDS,
+    message: "All price feeds:",
+    choices: ["abc"],
+  });
 });
 
 test(`${QUESTION_TEST_SUITE} - getSelectInitialValueQuestion`, () => {
